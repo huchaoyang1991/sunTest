544a80f154c70a2da03cb12861f0f95b5267de10
Sun <sun@163.com>
2017-08-11 00:05:28 +0800

    添gradle插件 支持testng

---
+++ src/main/java/com/sun/test/TestRun1.java
@@ -0,0 +1,13 @@
+package com.sun.test;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by huchaoyang on 2017/8/10.
+ */
+public class TestRun1 {
+    @Test
+    public void run(){
+        assert true;
+    }
+}
